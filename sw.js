149dc5209d549592309e84bd6ca626e85763225c
koerperstark <stadler.stefan@outlook.com>
2026-09-20 14:26:54 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 /* Anatomie — Offline-Cache */
-var V = 'anatomie-v1';
+var V = 'anatomie-v2';
 var SHELL = ['./', './index.html', './manifest.webmanifest',
              './icon-180.png', './icon-192.png', './icon-512.png'];
 
